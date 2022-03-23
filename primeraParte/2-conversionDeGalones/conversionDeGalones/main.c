a1be69877812a24ff086563bd8716f794f4e8b80
Josseth Rodriguez <hfernando382@gmail.com>
2022-03-23 09:46:33 -0400

    Agregando La conversion de galones

--- primeraParte/2-conversionDeGalones/conversionDeGalones/main.c
+++ primeraParte/2-conversionDeGalones/conversionDeGalones/main.c
@@ -7,13 +7,14 @@ int main()
     int galon;
     float litros;
 
+    printf("Este Programa mostrara la equivalencia entre galones y litros \n");
+    printf("partiendo de 10 unidades\n \n");
     printf("Galones(gal) \t\t litros(L) \n");
 
     for(galon = 10; galon <= 20; galon ++){
 
         litros = galon * 3.787;
         printf("%d gal \t\t=\t %f L \n", galon, litros);
-
     }
 
 
