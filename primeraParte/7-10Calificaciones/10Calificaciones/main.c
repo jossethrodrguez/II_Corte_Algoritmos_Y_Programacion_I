fee8eb8a7bbc4393fa7fe2df24f29bb278c210c1
Josseth Rodriguez <hfernando382@gmail.com>
2022-05-12 12:05:55 -0400

    adding the seventh exercie

--- primeraParte/7-10Calificaciones/10Calificaciones/main.c
+++ primeraParte/7-10Calificaciones/10Calificaciones/main.c
@@ -3,6 +3,29 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("Calculemos el promedio de 10 calificaciones\nes importante resaltar que solo tomare en cuenta las que esten entre 0 - 10\n");
+
+    int contador = 0, suma = 0, calificacion = 0;
+    float promedio = 0;
+
+    for (contador = 1; contador <= 10 ;contador++){
+
+        printf("\nIntroduce Calificacion %i: ", contador);
+        scanf("%i", &calificacion);
+
+        if (calificacion >= 0 && calificacion <= 10){
+            suma = suma + calificacion;
+        }
+
+    }
+
+    contador--; // esta es agregada porque el contador tiene uno adicional
+                // puede comprobarlo con el codigo que esta comentado en la linea 28
+
+    promedio = suma / contador;
+    printf("\nel promedio de las calificaciones fue : %f\n", promedio);
+
+    //printf("\n el valor del contador es : %i", contador);
+
     return 0;
 }
