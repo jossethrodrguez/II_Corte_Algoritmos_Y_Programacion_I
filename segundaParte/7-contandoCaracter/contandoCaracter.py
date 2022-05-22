8cea3477070c2ee5e5f55efd588dc870d20bf98a
Josseth Rodriguez <hfernando382@gmail.com>
2022-05-22 15:43:14 -0400

    adding the 7th exercies

---
+++ segundaParte/7-contandoCaracter/contandoCaracter.py
@@ -0,0 +1,6 @@
+# septimo Ejercicio - Segundo Corte | Josseth Rodriguez | II0411
+
+oracionDelUsuario = input("Escribe una palabra: ");
+letra = input("Especifica que letra quieres contar: ");
+
+print("la letra '",letra ,"' aparece ", oracionDelUsuario.count(letra), "veces");
